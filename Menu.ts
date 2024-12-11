452518820c03366162e37ae05a8164abec16f4f7
Rafael Queiróz <60499241+rafaelq80@users.noreply.github.com>
2024-12-11 10:30:33 -0300

    :sparkles: Classe Model Conta

--- Menu.ts
+++ Menu.ts
@@ -1,9 +1,25 @@
 import readlinesync = require("readline-sync");
 import { colors } from './src/util/Colors';
+import { Conta } from "./src/model/Conta";
 
 export function main() {
 
     let opcao: number;
+
+    // Cria novas instâncias (Objetos) da Classe Conta
+    const c1 = new Conta(1, 123, 1, "Jonas", 100000);
+    c1.visualizar();
+
+    // Saque
+    console.log(c1.sacar(200000.00));
+    c1.visualizar();
+
+    const c2 = new Conta(2, 123, 2, "Aline", 200000);
+    c2.visualizar();
+
+    // depósito
+    c2.depositar(100.00);
+    c2.visualizar();
 
     while (true) {
 
